be0dc0258d951502ac7ca643808666e4bfb1a0cf
Pjd123 <31391521+PJD123@users.noreply.github.com>
2019-04-13 14:08:33 -0500

    Create ptest.py
    Python file enumeration test

---
+++ ptest.py
@@ -0,0 +1,15 @@
+print()
+print("**************** Starting ***************")
+print()
+import os, sys, shutil
+srcPath = 'C:/Users/User/Documents'
+dstPath = 'C:/Users/User/testcopy'
+
+src = os.listdir(srcPath)
+dst = os.listdir(dstPath)
+
+# for file in src:
+#     print(file)
+
+for index, file in enumerate(src):
+    print(index, file)
